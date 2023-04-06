82176b13e54956f92bb66148b7a8c388dba6d2ba
Endre Ulberg <endre.ulberg@gmail.com>
2023-04-06 23:05:32 +0200

    rust handbook chapter 2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use colored::*;
+use rand::Rng;
+use std::{cmp::Ordering, io};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    loop {
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line.");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess.to_string());
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("{}", "Too small!".red()),
+            Ordering::Greater => println!("{}", "Too big!".red()),
+            Ordering::Equal => {
+                println!("{}", "You win!".green());
+
+                break;
+            }
+        }
+    }
 }
